16bb865a351dc1a70b70ffe9ccc4c494fd6f730c
Pranav Ambiga <pranav.ambig123@gmail.com>
2024-02-01 22:24:20 +0530

    updated config.sh

--- Hosting_config/config.sh
+++ Hosting_config/config.sh
@@ -1,9 +1,9 @@
 sudo apt-get update
 sudo apt-get install openjdk-8-jdk
-dos2unix  ../Distributed_Load_Testing_System/orchestrator.py
+dos2unix  ./Distributed_Load_Testing_System/orchestrator.py
 dos2unix  *.sh
 chmod +x *.sh
 source ./kafka.sh
 sudo systemctl start kafka
-pip3 install -r ../requirements.txt
-../Distributed_Load_Testing_System/orchestrator.py
+pip3 install -r ./requirements.txt
+./Distributed_Load_Testing_System/orchestrator.py
