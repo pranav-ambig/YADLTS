7f5ff1eb49978f0d83cd68ba87aae08ca527d5f1
Rahul  Rao <rahulrrao2002@gmail.com>
2024-02-03 23:50:16 +0530

    Update config.sh

--- Hosting_config/config.sh
+++ Hosting_config/config.sh
@@ -3,4 +3,5 @@ dos2unix  *.sh
 chmod +x *.sh
 source ./kafka.sh
 pip3 install -r ./requirements.txt
+chmod +x ./Distributed_Load_Testing_System/orchestrator.py
 python3 ./Distributed_Load_Testing_System/orchestrator.py
