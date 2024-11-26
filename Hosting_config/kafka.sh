6673b704c0652f157c63cca5aa6993013a30306d
rahulrao9 <rahulrrao2002@gmail.com>
2024-11-26 10:52:53 +0530

    kafka version revert

--- Hosting_config/kafka.sh
+++ Hosting_config/kafka.sh
@@ -15,12 +15,12 @@ fi
 
 # Download Kafka
 echo "Downloading Kafka binaries"
-wget https://downloads.apache.org/kafka/3.6.2/kafka_2.13-3.6.2.tgz
+wget https://archive.apache.org/dist/kafka/3.6.0/kafka_2.13-3.6.0.tgz
 
 # Extract Kafka
 echo "Decompressing tar archive for Kafka"
-tar -xf kafka_2.13-3.6.2.tgz
-mv kafka_2.13-3.6.2 /usr/local/kafka
+tar -xf kafka_2.13-3.6.0.tgz
+mv kafka_2.13-3.6.0 /usr/local/kafka
 
 # Setting up zookeeper.service
 echo "Setting up zookeeper.service"
